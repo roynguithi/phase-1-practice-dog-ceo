2662ce534921ced2ed8e42548827fc926d14ef6f
roy collins nguithi <roynguithi@student.moringaschool.com>
2024-07-15 16:32:33 +0300

    phase-1-practice-dog-ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('%c HI', 'color: firebrick')
+//dog images API
+document.addEventListener('DOMContentLoaded',()=>{
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const dogImagesContainer = document.getElementById('dog-images');
+    
+    fetch(umgUrl)
+         .then(response = response.json())
+         .then(data => {
+            data.message.foreach(imgUrl => {
+                const img = document.createElement('img');
+                img.src=imgUrl;
+                dogImagesContainer.appendChild(img);
+            })
+         })
+         . catch(error => {
+              console.error('Error fetching images:',error);
+         })
+})
+
+
+document.addEventListener('DOMContentLoaded', =>{
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
+    const breedList = document.getElementById('breed-list')
+        
+    fetch(breedUrl)
+    .then(response = response.json());
+    .then(data =>{
+        const breeds = Object.keys(data.message);
+        breeds.forEach(breed => {
+            const li = document.createElement('li');
+            li.textContent = breed;
+            breedList.appendChild(li);
+         })
+    })
+    .catch( error => {
+        console.error('error fetching breeds:',error);
+    });
+});  
